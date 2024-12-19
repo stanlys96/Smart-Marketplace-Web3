7263ec37a4d9f45036287f8fccf72fb07360b6bd
stanlys96 <stanlyskwok@gmail.com>
2024-12-20 03:44:24 +0700

    wlao

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ const nextConfig = {
   images: {
     domains: ["gateway.pinata.cloud"],
   },
+  transpilePackages: [
+    "antd",
+    "@ant-design",
+    "rc-util",
+    "rc-pagination",
+    "rc-picker",
+    "rc-notification",
+    "rc-tooltip",
+    "rc-tree",
+    "rc-table",
+  ],
 };
 
 export default nextConfig;
