8b0c3bac3e3db3414a92c83aee67f4ed200a79da
stanlys96 <stanlyskwok@gmail.com>
2024-12-20 03:29:29 +0700

    wlao

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["gateway.pinata.cloud"],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.m?js$/,
+      resolve: {
+        fullySpecified: false, // Avoid issues with ESM modules
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
