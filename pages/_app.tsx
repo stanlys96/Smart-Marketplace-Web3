d908670cad9e4b8549deabc1821fa19f09d5350d
stanlys96 <54090219+stanlys96@users.noreply.github.com>
2024-12-20 11:39:07 +0700

    Update _app.tsx to only include lisk sepolia

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,7 @@ import { AppProps } from "next/app";
 import "../src/app/globals.css";
 import "../src/app/secondary.css";
 import { WagmiProvider } from "wagmi";
-import {
-  mainnet,
-  arbitrum,
-  optimism,
-  polygon,
-  lisk,
-  liskSepolia,
-} from "wagmi/chains";
+import { liskSepolia } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createWeb3Modal } from "@web3modal/wagmi";
 import { defaultWagmiConfig } from "@web3modal/wagmi";
@@ -21,13 +14,13 @@ const queryClient = new QueryClient({});
 const PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID;
 
 const metadata = {
-  name: "Gumroad",
-  description: "Gumroad",
-  url: "https://gumroad-web3.vercel.app",
+  name: "Smart Marketplace",
+  description: "Smart Marketplace",
+  url: "https://smart-marketplace-web3.vercel.app",
   icons: [process.env.NEXT_PUBLIC_ICON_URL ?? ""],
 };
 
-const chains: any = [mainnet, arbitrum, optimism, polygon, lisk, liskSepolia];
+const chains: any = [liskSepolia];
 const config = defaultWagmiConfig({
   chains,
   projectId: PROJECT_ID ?? "",
